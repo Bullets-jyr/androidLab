5b4813e8a4d6a814c8b003410a9883075e61131a
Bullets-jyr <jyr@bullets.co.kr>
2023-01-21 11:22:45 +0900

    06-2-1-뷰 클래스 - 기본구조, 레이아웃 뷰 이용하기

---
+++ lab6/src/main/java/kr/co/bullets/lab6/test2/Test3Activity.kt
@@ -0,0 +1,12 @@
+package kr.co.bullets.lab6.test2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.bullets.lab6.R
+
+class Test3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test3)
+    }
+}
