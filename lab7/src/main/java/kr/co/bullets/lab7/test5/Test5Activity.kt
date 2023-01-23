1389e89ee7c049e0c8b56f6ccd6ab61381bc9aa2
Bullets-jyr <jyr@bullets.co.kr>
2023-01-23 16:21:45 +0900

    07-5-계층 구조로 배치 - ConstraintLayout

---
+++ lab7/src/main/java/kr/co/bullets/lab7/test5/Test5Activity.kt
@@ -0,0 +1,12 @@
+package kr.co.bullets.lab7.test5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.bullets.lab7.R
+
+class Test5Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test5)
+    }
+}
