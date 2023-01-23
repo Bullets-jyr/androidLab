3b483030ad05afd483ffa1bdf2392cec05a29c35
Bullets-jyr <jyr@bullets.co.kr>
2023-01-23 09:49:23 +0900

    06-3-기본적인 뷰 살펴보기

---
+++ lab6/src/main/java/kr/co/bullets/lab6/test4/Test5Activity.kt
@@ -0,0 +1,12 @@
+package kr.co.bullets.lab6.test4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.bullets.lab6.R
+
+class Test5Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test5)
+    }
+}
