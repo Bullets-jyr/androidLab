a2b9a548004d19c983894f200dcad570ec2dfd06
Bullets-jyr <jyr@bullets.co.kr>
2023-01-23 15:54:50 +0900

    07-4-표 형태로 배치 - GridLayout

---
+++ lab7/src/main/java/kr/co/bullets/lab7/test4/Test4Activity.kt
@@ -0,0 +1,12 @@
+package kr.co.bullets.lab7.test4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.bullets.lab7.R
+
+class Test4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test4)
+    }
+}
