2839c8f66e060dbe9df8230cfdf5a1f3395115a1
Bullets-jyr <jyr@bullets.co.kr>
2023-01-24 15:20:34 +0900

    09-1-리소스의 종류와 특징

---
+++ lab9/src/main/java/kr/co/bullets/lab9/test1/Test1Activity.kt
@@ -0,0 +1,12 @@
+package kr.co.bullets.lab9.test1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.bullets.lab9.R
+
+class Test1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test1)
+    }
+}
