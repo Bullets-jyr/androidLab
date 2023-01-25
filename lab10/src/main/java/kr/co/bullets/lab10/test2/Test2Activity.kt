4067c1a0111ec31c0d1405596ccb93e3a24facbb
Bullets-jyr <jyr@bullets.co.kr>
2023-01-25 11:09:00 +0900

    10-3-1-다양한 다이얼로그 - 토스트, 시간, 날짜 다이얼로그

---
+++ lab10/src/main/java/kr/co/bullets/lab10/test2/Test2Activity.kt
@@ -0,0 +1,56 @@
+package kr.co.bullets.lab10.test2
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.DatePicker
+import android.widget.TimePicker
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import kr.co.bullets.lab10.R
+import kr.co.bullets.lab10.databinding.ActivityTest2Binding
+
+class Test2Activity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityTest2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+            val toast = Toast.makeText(this, "종료하려면 한번 더 누르세요...", Toast.LENGTH_SHORT)
+            toast.addCallback(object : Toast.Callback() {
+                override fun onToastHidden() {
+                    super.onToastHidden()
+                    Log.d("kkang", "toast hidden...")
+                }
+
+                override fun onToastShown() {
+                    super.onToastShown()
+                    Log.d("kkang", "toast show...")
+                }
+            })
+            toast.show()
+        }
+
+        binding.button2.setOnClickListener {
+            DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                    Log.d("kkang", "data: $year, $month, $dayOfMonth")
+                }
+            }, 2022, 10, 3).show()
+        }
+
+        binding.button3.setOnClickListener {
+            TimePickerDialog(this, object : TimePickerDialog.OnTimeSetListener {
+                override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+                    Log.d("kkang", "time: $hourOfDay, $minute")
+                }
+            }, 15, 0, true).show()
+        }
+    }
+}
