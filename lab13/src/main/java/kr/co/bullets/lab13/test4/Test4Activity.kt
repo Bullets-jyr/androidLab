100eaf6b060a7da7b3ab6d7e5be78c5fbd8f4af4
Bullets-jyr <jyr@bullets.co.kr>
2023-06-04 13:19:59 +0900

    13-3-액티비티 제어

---
+++ lab13/src/main/java/kr/co/bullets/lab13/test4/Test4Activity.kt
@@ -0,0 +1,31 @@
+package kr.co.bullets.lab13.test4
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import kr.co.bullets.lab13.R
+
+class Test4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test4)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.setDecorFitsSystemWindows(false)
+            val controller = window.insetsController
+            if(controller != null){
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+
+        }else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+}
